f3f55d467ff3fdda5d9b493bbfe3d472811a5344
Abdulloh072008 <abdullohbilol54@gmail.com>
2026-03-08 12:22:35 +0500

    class work

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import "./style/App.css"
+import Card from './companent/card'
+import Button from './companent/Button'
+import user from './assets/image.png'
 
 const App = () => {
   return (
     <div>
-      
+      <Card img={user} name="My card" about="I am Abdulloh i am learning react" color={"red"}>
+      <Button />
+      </Card>
     </div>
   )
 }
